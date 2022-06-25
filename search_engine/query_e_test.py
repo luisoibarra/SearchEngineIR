3546492428a8d0e4744878ded4ea2792d2e167ec
AdrianHP <adrianmatcom@gmail.com>
2022-06-25 14:46:22 -0400

    get query expandn works

--- search_engine/query_e_test.py
+++ search_engine/query_e_test.py
@@ -1,6 +1,7 @@
 
 from itertools import count
 from pkgutil import get_data
+from unittest import result
 from search_logic.ir_models.base import *
 from pathlib import Path
 import numpy as np
@@ -17,7 +18,7 @@ def generate_data():
     return read_documents_from_hard_drive(context)
 
   
-def get_words_dict(contex:dict):
+def generate_words_dict(contex:dict):
     words = {""}
     
     docs = contex["documents"]
@@ -30,34 +31,67 @@ def get_words_dict(contex:dict):
     for i,word in enumerate(words):
         words_dict[word] = i
     
+    word_list = list(words)
+    
     contex["words_dict"] = words_dict
+    context["words_list"] = word_list
+    save_object(["words_dict"],words_dict, "words_dict")
+    save_object (["words_list"],word_list, "words_list")
     
     return contex
   
-def generate_sparce_matrix(contex:dict):
+def generate_sparse_matrix(contex:dict):
     
     docs = contex["documents"]
-    dict = contex["words_dict"]
+    words_dict = contex["words_dict"]
+   
+    
     
     sparse_matrix = csr_matrix((SIZE, SIZE), dtype = np.int8)
     count = 0
     for doc in docs:
         text_split = doc["text"].split()
         for i in range(len(text_split)-1):
-            sparse_matrix[dict[text_split[i]], dict[text_split[i+1]]] += 1
+            sparse_matrix[words_dict[text_split[i]], words_dict[text_split[i+1]]] += 1
         count +=1 
         print(str(count) + " documentos procesados ")
             
     contex["sparse_matrix"] = sparse_matrix
     save_object(["sparse_matrix"],sparse_matrix, "sparse_matrix")
-    print (sparse_matrix)
+   
     return contex
-            
+
+def get_expand_query(contex:dict,word:str):
+    dict = contex["words_dict"]
+    words_list = context["words_list"]
+    sparse_m = contex["sparse_matrix"]
+    if(word in dict):
+        word_index = dict[word]
+        word_row = sparse_m[word_index]
+        non_zero_values = word_row.nonzero()[1]
+        rank_words = []
+        for val in non_zero_values:
+            rank_words.append([word_row[0,val],val])
+        
+        rank_words.sort(reverse=True)    
+        
+        result = [words_list[i[1]] for i in rank_words]
+        
+        return result
+    
+    
+    else: 
+        return []
+          
             
     
 context = generate_data()
-context["words_dict"] = get_object(["Dict of words on the Corpus"],"words_dict")
-context = generate_sparce_matrix(context)
+context["words_dict"] = get_object(["words_dict"],"words_dict")
+context["sparse_matrix"] = get_object(["sparse_matrix"],"sparse_matrix")
+context["words_list"] = get_object(["words_list"],"words_list")
+
+list = get_expand_query(context,"air")
+print (context["sparse_matrix"])
     
 
      
